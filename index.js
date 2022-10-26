44f04f682c55a5e283a2cc0b69a9051f843bc5b7
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-10-26 06:35:51 -0700

    Necessary api's are created

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const cors = require('cors');
 
+const courses = require('./Data/courses.json');
+
 app.use(cors());
 
 const Port = process.env.Port || 5000;
@@ -11,6 +13,16 @@ const Port = process.env.Port || 5000;
 
 app.get('/', (req, res) => {
     res.send("Now server is running");
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const getCourse = courses.find(course => course.id == id);
+    res.send(getCourse);
 })
 
 app.listen(Port, () => {
